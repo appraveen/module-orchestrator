2ad108aadf660067d0b1c69fc6671bf4a314e616
Praveen Athmanathan <pathmanathan@ebay.com>
2015-01-19 17:26:21 -0800

    fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,5 +28,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-simple-mocha');
 
-	grunt.registerTask('default', ['jshint', 'simplemocha']);
+	grunt.registerTask('default', ['jshint']);
 };
