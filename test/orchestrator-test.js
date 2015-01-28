d5fbea6756b5345d7f685bc2c260c291fcb57cad
Praveen Athmanathan <pathmanathan@ebay.com>
2015-01-28 03:43:18 -0800

    resolver test case

--- test/orchestrator-test.js
+++ test/orchestrator-test.js
@@ -3,7 +3,7 @@
 var chai = require('chai'),
 	assert = chai.assert,
 	should = chai.should(),
-	orchestrator = require('../lib/orchestrator'),
+	orchestrator = require('../lib/Orchestrator'),
 	helper = require('./modules/helper');
 
 
@@ -17,6 +17,36 @@ describe('orchestrator-test', function() {
 			console.log(Math.round((diff[0] * 1000) + (diff[1] / 1000000)));
 			done();
 		}, 2000);
+	});
+
+	it('execute module with moduleResolver', function(done) {
+		var ipmodules = {
+			'name': 'test',
+			'moduleResolver': function(moduleName) {
+				return (moduleName === 'ModuleOne') ?
+					require('./modules/TasksOdd').One :
+					require('./modules/TasksEven').Two; 
+			},
+			'modules': {
+				'ModuleOne': [],
+				'ModuleTwo': ['ModuleOne']
+			}
+		};
+		//Module Name that does not exists in input configuration is decaled as bad
+		//dependency and the original module is still executed by discarding 
+		//bad depdencies. In this case '1' is ignored.
+
+		//var obj = helper.constructObject('orch test 1', ipmodules);
+		var orch = new orchestrator(ipmodules);
+
+		orch.start(function(err, results) {
+			console.log(results);
+			assert.typeOf(results, 'object');
+			results.ModuleOne.v.should.equal(1);
+			results.ModuleTwo.v.should.equal(2);
+			done();
+		});
+
 	});
 
 	it('execute module with bad dependency', function(done) {
